00be49146ac49cc4a28a7b1b1545a6ceabf064f5
EduardoReis7 <eduardodovigireis@gmail.com>
2020-04-05 15:40:46 -0300

    Add classe DetalhesTopico

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -8,12 +8,14 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.alura.forum.dto.DetalhesTopicoDTO;
 import br.com.alura.forum.dto.TopicoDTO;
 import br.com.alura.forum.form.TopicoForm;
 import br.com.alura.forum.modelo.Topico;
@@ -50,5 +52,12 @@ public class TopicosController {
 		
 		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
 		return ResponseEntity.created(uri).body(new TopicoDTO(topico));
+	}
+	
+	@GetMapping("/{id}")
+	public DetalhesTopicoDTO detalhar(@PathVariable Long id) {
+		
+		Topico topico = topicoRepository.getOne(id);
+		return new DetalhesTopicoDTO(topico);
 	}
 }
